f92d0e768e4e1b2301510784ecc4ed1f9df1287b
Jason Parks <>
2023-11-02 03:10:48 -0500

    .

---
+++ docker/docker_entrypoint.py
@@ -0,0 +1,146 @@
+#!/usr/local/bin/python
+from os import path, symlink, system, chown, setgid, setuid, environ, stat, walk, makedirs, remove
+from distutils.file_util import copy_file
+from pwd import getpwuid
+from subprocess import Popen, STDOUT
+from importlib import util as import_util
+from shutil import move
+
+
+BASE_DIR = path.abspath(environ.get("APP_DIR"))
+FILES_DIR = path.abspath(environ.get("FILES_DIR"))
+IGUANA_DIR = path.join(BASE_DIR, "src")
+
+IGUANA_PUID = int(environ.get("PUID"))
+IGUANA_PGID = int(environ.get("PGID"))
+
+VARIANT = environ.get("VARIANT")
+USE_NGINX = environ.get("USE_NGINX").lower() in ("true", "1")
+
+SETTING_TIME_ZONE = environ.get("TZ")
+SETTING_LANGUAGE = environ.get("LANG")
+
+FIRST_RUN_FILE = path.join(FILES_DIR, ".initialized")
+FORCE_CHOWN = False
+
+
+# prepare the /files directory
+_iguana_files_dir = path.join(BASE_DIR, "files")
+# rename the existing /BASE_DIR/files directory to /BASE_DIR/files_default
+if not path.islink(_iguana_files_dir):
+    move(_iguana_files_dir, path.join(BASE_DIR, "files_default"))
+
+    # symlink /files to /BASE_DIR/files
+    symlink(FILES_DIR, _iguana_files_dir)
+
+# after the previous step files dir is moved to /BASE_DIR/files_default
+_iguana_files_dir = path.join(BASE_DIR, "files_default")
+
+# these files must exist everytime when the Docker container is started
+print("Creating default files on the volume.")
+_necessary_files = ["logs", path.join("media", "avatars", "default.svg")]
+# the settings.json file is also mandatory if we're not in development mode
+if VARIANT != "development":
+    _necessary_files.append("settings.json")
+# if Nginx is installed, the config is also necessary
+if USE_NGINX:
+    _necessary_files.append("nginx.conf")
+for df in _necessary_files:
+    # copy the file if it doesn't exists or create the directory
+    if not path.exists(path.join(FILES_DIR, df)):
+        src_path = path.join(_iguana_files_dir, df)
+        if path.isdir(src_path):
+            makedirs(path.join(FILES_DIR, df), exist_ok=True)
+        else:
+            makedirs(path.dirname(path.join(FILES_DIR, df)), exist_ok=True)
+            copy_file(src_path, path.join(FILES_DIR, df))
+
+        # force a chown after a file was copied
+        FORCE_CHOWN = True
+
+        # force reinitialization of the settings later if the settings.json file was recovered
+        if df == "settings.json" and path.isfile(FIRST_RUN_FILE):
+            remove(FIRST_RUN_FILE)
+
+
+# create Iguana user
+try:
+    # check if it exists
+    getpwuid(IGUANA_PUID)
+    print("User 'iguana' already exists.")
+except Exception:
+    print("Creating user 'iguana'.")
+    system("addgroup --gid " + str(IGUANA_PGID) + " iguana")
+    system("adduser --disabled-password --no-create-home --gecos '' --uid " + str(IGUANA_PUID) + " -G iguana iguana")
+
+
+# chown the application path to the Iguana user
+print('Setting file permissions.')
+for dir in [BASE_DIR, FILES_DIR]:
+    if stat(dir).st_uid != IGUANA_PUID or \
+            stat(dir).st_gid != IGUANA_PGID or \
+            FORCE_CHOWN:
+        # recursive chown
+        for root, dirs, files in walk(dir):
+            for d in dirs:
+                chown(path.join(root, d), IGUANA_PUID, IGUANA_PGID, follow_symlinks=False)
+            for f in files:
+                chown(path.join(root, f), IGUANA_PUID, IGUANA_PGID, follow_symlinks=False)
+
+        # don't forget the root directory
+        chown(dir, IGUANA_PUID, IGUANA_PGID, follow_symlinks=False)
+
+# start helper processes as root
+if VARIANT != "development":
+    if USE_NGINX:
+        # start nginx if it is used
+        Popen(["nginx", "-c", path.join(FILES_DIR, "nginx.conf")])
+
+    # start cron for logrotate
+    Popen(["crond", "-L", path.join(FILES_DIR, "logs", "cron.log")])
+
+
+# switch to user iguana
+setgid(IGUANA_PGID)
+setuid(IGUANA_PUID)
+
+
+# reinitialize settings on first run
+if not path.isfile(FIRST_RUN_FILE):
+    print("Initializing Iguana.")
+
+    # load side _side_module
+    _spec = import_util.spec_from_file_location('manage_settings',
+                                                path.join(IGUANA_DIR, "lib", "manage_settings.py"))
+    _side_module = import_util.module_from_spec(_spec)
+    _spec.loader.exec_module(_side_module)
+
+    # recreate the django secret key (override!)
+    _django_settings_file = path.join(IGUANA_DIR, "common", "settings", "__init__.py")
+    _iguana_settings_file = path.join(FILES_DIR, "settings.json")
+    _is_development = VARIANT == "development"
+    _side_module.initialize_secret_key(_django_settings_file, _iguana_settings_file, _is_development, True)
+
+    # set additional settings
+    _settings = {
+        'TIME_ZONE': SETTING_TIME_ZONE,
+        'LANGUAGE_CODE': SETTING_LANGUAGE
+    }
+    if _is_development:
+        _side_module.set_django_settings(_django_settings_file, _settings)
+    else:
+        _side_module.set_django_settings(_iguana_settings_file, _settings)
+
+    # mark as reinitialized
+    open(FIRST_RUN_FILE, 'x').close()
+
+
+# start Iguana
+print("Starting Iguana.")
+# apply migrations first (this may be necessary after an update, or a database change)
+system("python " + path.join(IGUANA_DIR, "make.py") + " migrations apply")
+
+if VARIANT == "development":
+    system("python " + path.join(IGUANA_DIR, "make.py") + " run 0.0.0.0:8000")
+else:
+    system("python " + path.join(IGUANA_DIR, "make.py") + " run")
---
+++ src/gitintegration/testcases/test_model_views.py
@@ -0,0 +1,409 @@
+"""
+Iguana (c) by Marc Ammon, Moritz Fickenscher, Lukas Fridolin,
+Michael Gunselmann, Katrin Raab, Christian Strate
+
+Iguana is licensed under a
+Creative Commons Attribution-ShareAlike 4.0 International License.
+
+You should have received a copy of the license along with this
+work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+"""
+from django.test import TestCase
+from django.urls import reverse
+import os
+import stat
+import tempfile
+from django.core.files import File
+
+from gitintegration.views import RepositoryDetailView, RepositoryListView
+from gitintegration.models import Repository
+from project.models import Project
+from django.contrib.auth import get_user_model
+
+from common.settings.common import REPOSITORY_ROOT, BASE_DIR
+from django.core.exceptions import ValidationError
+
+
+class GitIntegrationTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # NOTE: if you modify those elements they need to be created in setUp, instead of here
+        cls.user = get_user_model().objects.create_user('a', 'b', 'c')
+
+    def setUp(self):
+        self.client.force_login(self.user)
+        # NOTE: this element gets modified by some of those tests, so this shall NOT be created in setUpTestData()
+        self.project = Project(creator=self.user, name_short='PRJ')
+        self.project.save()
+
+    def test_path_validator(self):
+        filecontent1 = 'Hello World File 1'
+        tmp1 = tempfile.NamedTemporaryFile(delete=False)
+        tmp1.write(filecontent1.encode())
+        tmp1.close()
+        filecontent2 = 'Hello World File 2'
+        tmp2 = tempfile.NamedTemporaryFile(delete=False)
+        tmp2.write(filecontent2.encode())
+        tmp2.close()
+
+        with open(tmp1.name, 'r') as f1:
+            with open(tmp2.name, 'r') as f2:
+                response = self.client.post(reverse('project:gitintegration:create',
+                                                    kwargs={'project': self.project.name_short}
+                                                    ),
+                                            {'url': 'blubber-url',
+                                             'rsa_priv_path': f1,
+                                             'rsa_pub_path': f2,
+                                             },
+                                            follow=True)
+
+        repo = Repository(project=self.project, url='')
+
+        with open(tmp1.name, 'r') as f1:
+            with open(tmp2.name, 'r') as f2:
+                repo.rsa_priv_path = File(f1)
+                repo.rsa_pub_path = File(f2)
+
+                tests = ['',
+                         'blub',
+                         'bla://great.url/path',
+                         'bla://../../letstestthis',
+                         REPOSITORY_ROOT,
+                         BASE_DIR,
+                         os.path.join(REPOSITORY_ROOT, 'foreignRepo_1'),
+                         os.path.join(BASE_DIR, 'dir'),
+                         '/opt/repo_path'
+                         ]
+
+                for t in tests:
+                    repo.url = t
+                    self.assertRaises(ValidationError, repo.clean_fields)
+                    try:
+                        repo.clean_fields()
+                    except ValidationError as e:
+                        self.assertIn('url', e.message_dict.keys())
+
+        # cleanup tmp-files
+        os.unlink(tmp1.name)
+        os.unlink(tmp2.name)
+
+    def test_add_edit_list_detail_delete_allowed_for_manager(self):
+        self.project.manager.add(self.user)
+        self.project.save()
+
+        filecontent1 = 'Hello World File 1'
+        tmp1 = tempfile.NamedTemporaryFile(delete=False)
+        tmp1.write(filecontent1.encode())
+        tmp1.close()
+        filecontent2 = 'Hello World File 2'
+        tmp2 = tempfile.NamedTemporaryFile(delete=False)
+        tmp2.write(filecontent2.encode())
+        tmp2.close()
+
+        with open(tmp1.name, 'r') as f1:
+            with open(tmp2.name, 'r') as f2:
+                response = self.client.post(reverse('project:gitintegration:create',
+                                                    kwargs={'project': self.project.name_short}
+                                                    ),
+                                            {'url': 'git://blubber.url',
+                                             'rsa_priv_path': f1,
+                                             'rsa_pub_path': f2,
+                                             },
+                                            follow=True)
+
+        self.assertNotContains(response, "Your account doesn't have access to this page")
+        self.project.refresh_from_db()
+        self.assertEqual(self.project.repos.count(), 1)
+        repo = self.project.repos.first()
+        self.assertEqual(repo.url, 'git://blubber.url')
+        self.assertEqual(repo.__str__(), "Repository git://blubber.url")
+        self.assertIn(filecontent1, repo.rsa_priv_path.read().decode())
+        self.assertIn(filecontent2, repo.rsa_pub_path.read().decode())
+        # read() opened the file
+        repo.rsa_priv_path.close()
+        repo.rsa_pub_path.close()
+
+        # check file permissions, we want 600
+        self.assertEqual(oct(stat.S_IMODE(os.stat(repo.rsa_priv_path.path).st_mode)), '0o600')
+        self.assertEqual(oct(stat.S_IMODE(os.stat(repo.rsa_pub_path.path).st_mode)), '0o600')
+
+        # for some reason these paths gets modified during the testcase
+        private_path = repo.rsa_priv_path.path
+        public_path = repo.rsa_pub_path.path
+
+        with open(tmp1.name, 'r') as f1:
+            with open(tmp2.name, 'r') as f2:
+                response = self.client.post(reverse('project:gitintegration:edit',
+                                                    kwargs={'project': self.project.name_short,
+                                                            'repository': '1'}
+                                                    ),
+                                            {'url': 'git://blubber.url',
+                                             'rsa_priv_path': f2,
+                                             'rsa_pub_path': f1,
+                                             },
+                                            follow=True)
+        self.assertNotContains(response, "Your account doesn't have access to this page")
+        self.project.refresh_from_db()
+        self.assertEqual(self.project.repos.count(), 1)
+        repo = self.project.repos.first()
+        self.assertEqual(repo.url, 'git://blubber.url')
+        self.assertIn(filecontent2, repo.rsa_priv_path.read().decode())
+        self.assertIn(filecontent1, repo.rsa_pub_path.read().decode())
+        # read() opened the file
+        repo.rsa_priv_path.close()
+        repo.rsa_pub_path.close()
+
+        # check file permissions after edit, we want 600
+        self.assertEqual(oct(stat.S_IMODE(os.stat(repo.rsa_priv_path.path).st_mode)), '0o600')
+        self.assertEqual(oct(stat.S_IMODE(os.stat(repo.rsa_pub_path.path).st_mode)), '0o600')
+
+        # test list view
+        response = self.client.get(reverse('project:gitintegration:list',
+                                           kwargs={'project': self.project.name_short}
+                                           ))
+        self.assertEqual(response.context_data.get('object_list').count(), 1)
+        self.assertEqual(type(response.context_data.get('view')), RepositoryListView)
+        self.assertNotContains(response, "Your account doesn't have access to this page")
+
+        # test detail view
+        response = self.client.get(reverse('project:gitintegration:detail',
+                                           kwargs={'project': self.project.name_short,
+                                                   'repository': '1'}
+                                           ))
+        self.assertEqual(response.context_data.get('repository'), repo)
+        self.assertEqual(type(response.context_data.get('view')), RepositoryDetailView)
+        self.assertNotContains(response, "Your account doesn't have access to this page")
+
+        # test delete view
+        repo.refresh_from_db()
+        response = self.client.post(reverse('project:gitintegration:delete',
+                                            kwargs={'project': self.project.name_short,
+                                                    'repository': '1'}
+                                            ),
+                                    {'delete': 'true'},
+                                    follow=True)
+        self.assertEqual(Repository.objects.filter(project__pk=self.project.pk,
+                                                   pk=1).count(), 0)
+        self.assertEqual(os.path.isfile(repo.rsa_pub_path.path), False)
+        self.assertEqual(os.path.isfile(repo.rsa_priv_path.path), False)
+        self.assertEqual(os.path.isdir(repo.get_local_repo_path()), False)
+
+        # delete the key files from the server
+        os.unlink(private_path)
+        os.unlink(public_path)
+        # delete the key files locally
+        os.unlink(tmp1.name)
+        os.unlink(tmp2.name)
+
+    def test_add_edit_delete_not_allowed_for_developer(self):
+        self.project.manager.clear()
+        self.project.developer.add(self.user)
+        self.project.save()
+
+        response = self.client.post(reverse('project:gitintegration:create',
+                                            kwargs={'project': self.project.name_short}
+                                            ),
+                                    {'url': 'blubber-url',
+                                     'rsa_priv_path': None,
+                                     'rsa_pub_path': None,
+                                     },
+                                    follow=True)
+        self.assertContains(response, "Your account doesn't have access to this page")
+        self.project.refresh_from_db()
+        self.assertEqual(self.project.repos.count(), 0)
+
+        filecontent1 = 'Hello World File 1'
+        tmp1 = tempfile.NamedTemporaryFile(delete=False)
+        tmp1.write(filecontent1.encode())
+        tmp1.close()
+        filecontent2 = 'Hello World File 2'
+        tmp2 = tempfile.NamedTemporaryFile(delete=False)
+        tmp2.write(filecontent2.encode())
+        tmp2.close()
+
+        repo = Repository(project=self.project, url='blub-url')
+
+        with open(tmp1.name, 'r') as f1:
+            with open(tmp2.name, 'r') as f2:
+                repo.rsa_priv_path = File(f1)
+                repo.rsa_pub_path = File(f2)
+                repo.save()
+
+        response = self.client.post(reverse('project:gitintegration:edit',
+                                            kwargs={'project': self.project.name_short,
+                                                    'repository': '1'}
+                                            ),
+                                    {'url': 'blubber',
+                                     'rsa_priv_path': None,
+                                     'rsa_pub_path': None,
+                                     },
+                                    follow=True)
+        self.assertContains(response, "Your account doesn't have access to this page")
+        self.project.refresh_from_db()
+        self.assertEqual(self.project.repos.count(), 1)
+        self.assertEqual(self.project.repos.first().url, 'blub-url')
+
+        response = self.client.post(reverse('project:gitintegration:delete',
+                                            kwargs={'project': self.project.name_short,
+                                                    'repository': '1'}
+                                            ),
+                                    {'delete': 'true'},
+                                    follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Your account doesn't have access to this page")
+        self.project.refresh_from_db()
+        self.assertEqual(self.project.repos.count(), 1)
+        self.assertEqual(self.project.repos.first().url, 'blub-url')
+
+        # delete the key files from the server
+        repo = self.project.repos.first()
+        os.unlink(repo.rsa_priv_path.path)
+        os.unlink(repo.rsa_pub_path.path)
+        # delete the key files locally
+        os.unlink(tmp1.name)
+        os.unlink(tmp2.name)
+
+    def test_keep_and_not_delete_repository(self):
+        filecontent1 = 'Hello World File 1'
+        tmp1 = tempfile.NamedTemporaryFile(delete=False)
+        tmp1.write(filecontent1.encode())
+        tmp1.close()
+        filecontent2 = 'Hello World File 2'
+        tmp2 = tempfile.NamedTemporaryFile(delete=False)
+        tmp2.write(filecontent2.encode())
+        tmp2.close()
+
+        repo = Repository(project=self.project, url='')
+        with open(tmp1.name, 'r') as f1:
+            with open(tmp2.name, 'r') as f2:
+                repo.rsa_priv_path = File(f1)
+                repo.rsa_pub_path = File(f2)
+                repo.save()
+
+        response = self.client.post(reverse('project:gitintegration:delete',
+                                            kwargs={'project': self.project.name_short,
+                                                    'repository': '1'}
+                                            ),
+                                    {'keep': 'true'},
+                                    follow=True)
+        self.assertEqual(self.project.repos.count(), 1)
+
+        # delete the key files from the server
+        os.unlink(repo.rsa_priv_path.path)
+        os.unlink(repo.rsa_pub_path.path)
+        # delete the key files locally
+        os.unlink(tmp1.name)
+        os.unlink(tmp2.name)
+
+    def test_detail_allowed_for_developer(self):
+        self.project.manager.clear()
+        self.project.developer.clear()
+        self.project.developer.add(self.user)
+        self.project.save()
+
+        filecontent1 = 'Hello World File 1'
+        tmp1 = tempfile.NamedTemporaryFile(delete=False)
+        tmp1.write(filecontent1.encode())
+        tmp1.close()
+        filecontent2 = 'Hello World File 2'
+        tmp2 = tempfile.NamedTemporaryFile(delete=False)
+        tmp2.write(filecontent2.encode())
+        tmp2.close()
+
+        repo = Repository(project=self.project, url='blub-url')
+
+        with open(tmp1.name, 'r') as f1:
+            with open(tmp2.name, 'r') as f2:
+                repo.rsa_priv_path = File(f1)
+                repo.rsa_pub_path = File(f2)
+                repo.save()
+
+        response = self.client.get(reverse('project:gitintegration:detail',
+                                           kwargs={'project': self.project.name_short,
+                                                   'repository': '1'}
+                                           ),
+                                   follow=True)
+        self.assertEqual(response.context_data.get('repository'), repo)
+        self.assertEqual(type(response.context_data.get('view')), RepositoryDetailView)
+        self.assertNotContains(response, "Your account doesn't have access to this page")
+        with open(tmp1.name, 'r') as f1:
+            with open(tmp2.name, 'r') as f2:
+                self.assertContains(response, str(f2.read()))
+                self.assertNotContains(response, str(f1.read()))
+
+        # delete the key files from the server
+        os.unlink(repo.rsa_priv_path.path)
+        os.unlink(repo.rsa_pub_path.path)
+        # delete the key files locally
+        os.unlink(tmp1.name)
+        os.unlink(tmp2.name)
+
+    def test_views_not_allowed_for_non_project_member(self):
+        filecontent1 = 'Hello World File 1'
+        tmp1 = tempfile.NamedTemporaryFile(delete=False)
+        tmp1.write(filecontent1.encode())
+        tmp1.close()
+        filecontent2 = 'Hello World File 2'
+        tmp2 = tempfile.NamedTemporaryFile(delete=False)
+        tmp2.write(filecontent2.encode())
+        tmp2.close()
+
+        repo = Repository(project=self.project, url='blub-url')
+        with open(tmp1.name, 'r') as f1:
+            with open(tmp2.name, 'r') as f2:
+                repo.rsa_priv_path = File(f1)
+                repo.rsa_pub_path = File(f2)
+                repo.save()
+
+        response = self.client.get(reverse('project:gitintegration:list',
+                                           kwargs={'project': self.project.name_short}
+                                           ),
+                                   follow=True)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.get(reverse('project:gitintegration:detail',
+                                           kwargs={'project': self.project.name_short,
+                                                   'repository': repo.pk}
+                                           ),
+                                   follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Your account doesn't have access to this page")
+
+        response = self.client.post(reverse('project:gitintegration:create',
+                                            kwargs={'project': self.project.name_short}
+                                            ),
+                                    {'url': 'blubber',
+                                     'rsa_priv_path': None,
+                                     'rsa_pub_path': None,
+                                     },
+                                    follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Your account doesn't have access to this page")
+
+        response = self.client.post(reverse('project:gitintegration:edit',
+                                            kwargs={'project': self.project.name_short,
+                                                    'repository': repo.pk}
+                                            ),
+                                    {'url': 'blubber',
+                                     'rsa_priv_path': None,
+                                     'rsa_pub_path': None,
+                                     },
+                                    follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Your account doesn't have access to this page")
+
+        response = self.client.post(reverse('project:gitintegration:delete',
+                                            kwargs={'project': self.project.name_short,
+                                                    'repository': repo.pk}
+                                            ),
+                                    {'delete': 'true'},
+                                    follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Your account doesn't have access to this page")
+
+        # delete the key files from the server
+        os.unlink(repo.rsa_priv_path.path)
+        os.unlink(repo.rsa_pub_path.path)
+        # delete the key files locally
+        os.unlink(tmp1.name)
+        os.unlink(tmp2.name)
